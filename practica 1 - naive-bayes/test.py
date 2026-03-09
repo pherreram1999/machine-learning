07654d6903576340c7153584b944a37b05f7a46d
Pedro Alonso <palhm.99@gmail.com>
2026-03-09 03:28:48 -0600

    se refactorizo el codigo para un mejor performance

--- practica 1 - naive-bayes/test.py
+++ practica 1 - naive-bayes/test.py
@@ -1,16 +1,34 @@
-from bayes import NaiveBayesDiscreto
+import argparse
+from NaiveBayesDiscreto import NaiveBayesDiscreto
+from NaiveBayesContinuo import NaiveBayesContinuo
 
 
 def discreto():
     nv = NaiveBayesDiscreto()
-    res = nv.predecir(['S','F','A' ,'F'])
-    print(res,res.max())
+    res = nv.predecir(['S', 'F', 'A', 'F'])
+    print(res)
+    print(nv.comprobar_por_restitucion())
 
-    pass
+
+def continuo():
+    nv = NaiveBayesContinuo()
+    res = nv.predecir([5.0, 3.4, 1.5, 0.2])
+    print(res)
+    print(nv.comprobar_por_restitucion())
+
 
 def main():
-    discreto()
-    pass
+    parser = argparse.ArgumentParser(description='Naive Bayes Classifier')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-d', '--discreto', action='store_true', help='Usar Naive Bayes Discreto')
+    group.add_argument('-c', '--continuo', action='store_true', help='Usar Naive Bayes Continuo')
+    args = parser.parse_args()
+
+    if args.discreto:
+        discreto()
+    elif args.continuo:
+        continuo()
+
 
 if __name__ == "__main__":
     main()
